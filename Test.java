0aa7af4b074911055ff0250fea9001a10ce17219
jpkale <jpkale@jpkserver.local>
2013-10-22 20:04:02 -0400

    Test working

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
 
-	public static void main(String[] args){
-		print(‘hello world’);
-	}
+	public static void main(String[] args) {
+        	System.out.println("Hello, World");
+    	}
 
 }
