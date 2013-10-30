dafd618064aa37d20d4f2221be34680e3e5767be
kitekid250 <wardprescott95@gmail.com>
2013-10-30 01:24:28 -0400

    Rotation Counter

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,22 +1,31 @@
 //Written by Ward Prescott - 2013 - For Bucknell MARC
 
-void setup(){
               int Aoutput = 2;
               int Boutput = 3;
-              pinMode(Aoutput, INPUT);
-              pinMode(Boutput, INPUT);
               long counter;
               long revs;
               long signedrevs;
-              int  ratio = 5;
-              word output;
-              boolean  old1;
-              boolean  old2;
-              boolean  new1;
-              boolean  new2;}
+              int  ratio = 64;
+              boolean  oldA;
+              boolean  oldB;
+              boolean  newA;
+              boolean  newB;
+
+void setup(){
+              pinMode(Aoutput, INPUT);
+              pinMode(Boutput, INPUT);
+              }
 
 void loop(){
-  
-  
-  when input one goes from zero to one or one to zero add rev
-  when input two goes from zero to one or one to zero add rev}
+              newA = digitalRead(Aoutput);
+              newB = digitalRead(Boutput);
+              if (newA != oldA){
+                counter = counter + 1;
+              };
+              if (newB != oldB){
+                counter = counter + 1;
+              };
+              oldA = newA;
+              oldB = newB;
+              revs = counter / ratio;
+            }
