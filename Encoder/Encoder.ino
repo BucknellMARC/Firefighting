a0d42870b27e8c62b30b1e774a5ae0ef0bad8d5d
kitekid250 <wardprescott95@gmail.com>
2013-10-30 01:23:32 -0400

    Rotation Counter
    Rotation counter code (untested), without any sort of direction
    determining factor

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,6 +1,22 @@
 //Written by Ward Prescott - 2013 - For Bucknell MARC
 
 void setup(){
-}
+              int Aoutput = 2;
+              int Boutput = 3;
+              pinMode(Aoutput, INPUT);
+              pinMode(Boutput, INPUT);
+              long counter;
+              long revs;
+              long signedrevs;
+              int  ratio = 5;
+              word output;
+              boolean  old1;
+              boolean  old2;
+              boolean  new1;
+              boolean  new2;}
 
-void loop(){}
+void loop(){
+  
+  
+  when input one goes from zero to one or one to zero add rev
+  when input two goes from zero to one or one to zero add rev}
