a391bab33b24632c15df62104b5836963f86c8b2
kitekid250 <wardprescott95@gmail.com>
2013-11-02 01:21:46 -0400

    Successfully Established Serial Communication
    Got the serial communication working between two Arduinos.  The TX
    transmits 4 integer values (and could do more if need be), and the RX
    receives them.  The changing of values mid program and the effect on the
    RX still needs testing.

--- Communication_RX/Communication_RX.ino
+++ Communication_RX/Communication_RX.ino
@@ -1,14 +1,42 @@
 /*Have this arduino communicate four values to the second arduino,
-a motor left value, a motor right value, a shooter value, and a fourth
-value for expandabilities sake.  The second arduino will then convert
-them into values useful for its motor controller and echo those values.
-Ideally two way communication would be added, though it should not be requisite*/
-
+ a motor left value, a motor right value, a shooter value, and a fourth
+ value for expandabilities sake.  The second arduino will then convert
+ them into values useful for its motor controller and echo those values.
+ Ideally two way communication would be added, though it should not be requisite*/
 //Created by Ward Prescott - November, 2013 for Bucknell MARC
 
-RECIEVER
+#include <EasyTransfer.h>
 
-void setup {
-            Serial.begin(9600);
+EasyTransfer ET;
+
+struct RECEIVE_DATA_STRUCTURE{
+  int motorl;
+  int motorr;
+  int motor3;
+  int value4;
+};
+
+RECEIVE_DATA_STRUCTURE mydata;
+
+void setup(){
+  int LED = 10;
+  Serial.begin(9600);
+  ET.begin(details(mydata), &Serial);
+  pinMode(LED, OUTPUT);
+
 }
-void loop {}
+
+void loop(){
+  if(ET.receiveData()){
+    if(mydata.value4 == 400){
+      digitalWrite(10, HIGH);
+            delay(10000);
+    }
+    
+  };
+  mydata.value4 = 0;
+  delay(250);
+}
+
+
+
--- Communication_TX/Communication_TX.ino
+++ Communication_TX/Communication_TX.ino
@@ -1,17 +1,33 @@
 /*Have this arduino communicate four values to the second arduino,
-a motor left value, a motor right value, a shooter value, and a fourth
-value for expandabilities sake.  The second arduino will then convert
-them into values useful for its motor controller and echo those values.
-Ideally two way communication would be added, though it should not be requisite*/
-
+ a motor left value, a motor right value, a shooter value, and a fourth
+ value for expandabilities sake.  The second arduino will then convert
+ them into values useful for its motor controller and echo those values.
+ Ideally two way communication would be added, though it should not be requisite*/
 //Created by Ward Prescott - November, 2013 for Bucknell MARC
 
-int motorl = 0
-int motorr = 0
-int motor3 = 0
-int value4 = 0
+#include <EasyTransfer.h>
+
+EasyTransfer ET; //Created Object
+
+struct SEND_DATA_STRUCTURE{ //creates data structure, and must be the same on both the TX and RX
+  int motorl;
+  int motorr;
+  int motor3;
+  int value4;
+};         
+
+SEND_DATA_STRUCTURE mydata;
 
 void setup (){
-            Serial.begin(9600);
+  Serial.begin(9600);
+  ET.begin(details(mydata), &Serial);
 }
-void loop (){}
+void loop (){
+  mydata.motorl = 10;
+  mydata.motorr = 200;
+  mydata.motor3 = 300;
+  mydata.value4 = 400;
+  ET.sendData();
+  delay(5000);
+}
+
