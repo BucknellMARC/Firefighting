5ac259a8848cb2a75dc3096afabc3ce77c1f9534
kitekid250 <wardprescott95@gmail.com>
2013-11-03 00:56:24 -0400

    Added Encoder Rev Counter and Tenative Pinmap
    Encoder code still needs testing

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,31 +1,69 @@
 //Written by Ward Prescott - 2013 - For Bucknell MARC
-
-              int Aoutput = 2;
-              int Boutput = 3;
-              long counter;
-              long revs;
-              long signedrevs;
-              int  ratio = 64;
-              boolean  oldA;
-              boolean  oldB;
-              boolean  newA;
-              boolean  newB;
+long     steps;
+long     oldsteps;
+long     revolutions;
+boolean  oldA;
+boolean  oldB;
+boolean  newA;
+boolean  newB;
+int Aoutput = 2;
+int Boutput = 3;
 
 void setup(){
-              pinMode(Aoutput, INPUT);
-              pinMode(Boutput, INPUT);
-              }
+  pinMode(Aoutput, INPUT);
+  pinMode(Boutput, INPUT);
+  Serial.begin(9600);
+}
 
 void loop(){
-              newA = digitalRead(Aoutput);
-              newB = digitalRead(Boutput);
-              if (newA != oldA){
-                counter = counter + 1;
-              };
-              if (newB != oldB){
-                counter = counter + 1;
-              };
-              oldA = newA;
-              oldB = newB;
-              revs = counter / ratio;
-            }
+  newA = digitalRead(Aoutput);
+  newB = digitalRead(Boutput);
+
+  if (oldA == 0 && newA == 1){
+    if (newB == 0){
+      steps = oldsteps + 1;
+      oldsteps = steps;
+    };
+    if (newB == 1){
+      steps = oldsteps - 1;
+      oldsteps = steps;
+    };
+  };
+  
+  
+  if (oldA == 1 && newA == 0){
+    if (newB == 0){
+      steps = oldsteps - 1;
+      oldsteps = steps;
+    };
+    if (newB == 1){
+      steps = oldsteps + 1;
+      oldsteps = steps;
+    };
+  };
+  
+  if (oldB == 0 && newB == 1){
+    if (newA == 0){
+      steps = oldsteps - 1;
+      oldsteps = steps;
+    };
+    if (newA == 1){
+      steps = oldsteps + 1;
+      oldsteps = steps;
+    };
+  };
+  
+  if (oldB == 1 && newB == 0){
+    if (newA == 0){
+      steps = oldsteps + 1;
+      oldsteps = steps;
+    };
+    if (newA == 1){
+      steps = oldsteps - 1;
+      oldsteps = steps;
+    };
+  };
+revolutions = steps / 64;  
+}
+
+
