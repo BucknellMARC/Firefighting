7fb6e7b97bea516ba9ceb4d9f23148800ce6a67a
jpkale <jpkale@jpkserver.local>
2013-11-05 20:07:54 -0500

    Add new motor control library

---
+++ Firefighter_2014/MotorControl.cpp
@@ -0,0 +1,105 @@
+//
+//  MotorControl.cpp
+//  Firefighter_2014
+//
+//  Created by Joseph Kale on 11/5/13.
+//
+//
+
+#include "MotorControl.h"
+#include "Arduino.h"
+#include "AFMotor.h"
+#include "MotorControl.h"
+
+AF_DCMotor motorLeft(3);
+AF_DCMotor motorRight(2);
+ENCODER_LEFT_ONE = 10
+ENCODER_LEFT_TWO = 11
+ENCODER_RIGHT_ONE = 6
+ENCODER_RIGHT_TWO = 9
+TURN_THRESHOLD = 153
+
+
+MotorControl::MotorControl(int leftForward, int rightForward, int leftReverse, int rightReverse){
+    Serial.begin(9600);
+    Serial.print("MotorControl initiated");
+    motorLeft.run(RELEASE);
+    motorRight.run(RELEASE);
+}
+
+void MotorControl::forward(){
+    motorLeft.run(FORWARD);
+    motorRight.run(FORWARD);
+}
+
+void forward(int distance){
+    forward();
+    int count = 0;
+    while (count < TURN_THRESHOLD * distance){
+        if (digitalRead(ENCODER_LEFT_ONE) && digitalRead(ENCODER_LEFT_TWO) &&
+            digitalRead(ENCODER_RIGHT_ONE) && digitalRead(ENCODER_RIGHT_TWO)){
+            count++;
+        }
+    }
+    brake();
+}
+
+void MotorControl::turnLeft(){
+    motorLeft.run(BACKWARD);
+    motorRight.run(FORWARD);
+}
+
+void MotorControl::turnLeft(int degree){
+    turnLeft();
+    int count = 0;
+    while (count < TURN_THRESHOLD * degree){
+        if (digitalRead(ENCODER_LEFT_ONE) && digitalRead(ENCODER_LEFT_TWO) &&
+            digitalRead(ENCODER_RIGHT_ONE) && digitalRead(ENCODER_RIGHT_TWO)){
+            count++;
+        }
+    }
+    brake();
+}
+
+void MotorControl::turnRight(){
+    motorLeft.run(FORWARD);
+    motorRight.run(BACKWARD);
+}
+
+void MotorControl::turnRight(int degree * degree){
+    turnRight();
+    int count = 0;
+    while (count < TURN_THRESHOLD){
+        if (digitalRead(ENCODER_LEFT_ONE) && digitalRead(ENCODER_LEFT_TWO) &&
+            digitalRead(ENCODER_RIGHT_ONE) && digitalRead(ENCODER_RIGHT_TWO)){
+            count++;
+        }
+    }
+    brake();
+}
+
+void MotorControl::brake(){
+    motorLeft.run(RELEASE);
+    motorRight.run(RELEASE);
+}
+
+void MotorControl::brake(int time){
+    delay(time);
+}
+
+void MotorControl::backward(){
+    motorLeft.run(BACKWARD);
+    motorRight.run(BACKWARD);
+}
+
+void MotorControl::backward(int distance){
+    backward();
+    int count = 0;
+    while (count < TURN_THRESHOLD * distance){
+        if (digitalRead(ENCODER_LEFT_ONE) && digitalRead(ENCODER_LEFT_TWO) &&
+            digitalRead(ENCODER_RIGHT_ONE) && digitalRead(ENCODER_RIGHT_TWO)){
+            count++;
+        }
+    }
+    brake();
+}
---
+++ Firefighter_2014/MotorControl.h
@@ -0,0 +1,37 @@
+//
+//  MotorControl.h
+//  Firefighter_2014
+//
+//  Created by Joseph Kale on 11/5/13.
+//
+//
+
+#ifndef __Firefighter_2014__MotorControl__
+#define __Firefighter_2014__MotorControl__
+
+#include <iostream>
+#include "Arduino.h"
+#include "AFMotor.h"
+
+class MotorControl{
+public:
+    MotorControl();
+    void forward();
+    void forward(int time);
+    void turnLeft();
+    void turnLeft(int time);
+    void turnRight();
+    void turnRight(int time);
+    void backward();
+    void backward(int time);
+    void brake();
+    void brake(int time);
+private:
+    int ENCODER_LEFT_ONE;
+    int ENCODER_LEFT_TWO;
+    int ENCODER_RIGHT_ONE;
+    int ENCODER_RIGHT_TWO;
+    int TURN_THRESHOLD;
+};
+
+#endif /* defined(__Firefighter_2014__MotorControl__) */
