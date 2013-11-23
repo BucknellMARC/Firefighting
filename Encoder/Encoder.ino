4cf74f2ac8359fd9b99589bbb882d2a46383b53f
kitekid250 <wardprescott95@gmail.com>
2013-11-23 01:03:57 -0500

    Encoder Working Again
    And this time it's optimized!!

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,17 +1,19 @@
 //Written by Ward Prescott - 2013 - For Bucknell MARC
 long     steps;
-long     oldsteps;
 long     revolutions;
 boolean  oldA;
 boolean  oldB;
 boolean  newA;
 boolean  newB;
-int Aoutput = 2;
-int Boutput = 3;
+int Aoutput = 3;
+int Boutput = 4;
+int button = 7;
+boolean state = 0;
 
 void setup(){
   pinMode(Aoutput, INPUT);
   pinMode(Boutput, INPUT);
+  pinMode(button, INPUT);
   Serial.begin(9600);
 }
 
@@ -21,49 +23,51 @@ void loop(){
 
   if (oldA == 0 && newA == 1){
     if (newB == 0){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
     if (newB == 1){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
   };
-  
-  
+
   if (oldA == 1 && newA == 0){
     if (newB == 0){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
     if (newB == 1){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
   };
-  
+
   if (oldB == 0 && newB == 1){
     if (newA == 0){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
     if (newA == 1){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
   };
-  
+
   if (oldB == 1 && newB == 0){
     if (newA == 0){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
     if (newA == 1){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
   };
-revolutions = steps / 64;  
+  oldA = newA;
+  oldB = newB;
+
+  revolutions = (steps / (64));  
+  state = digitalRead(button);
+
+  if (state == 1) {
+    Serial.print(revolutions);
+    delay(300);
+  };
+
 }
+
 
 
