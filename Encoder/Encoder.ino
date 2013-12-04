a53d4c8cf30dcaac2448ca5374e402f4ba78e436
jpkale <jpkale@jpkserver.local>
2013-12-03 19:56:40 -0500

    Revert "Revert "Revert "Revert "Commit""""
    This reverts commit 12c79b9db0c41e5a78603a8a246008d981cee2d4.

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,17 +1,19 @@
 //Written by Ward Prescott - 2013 - For Bucknell MARC
 long     steps;
-long     oldsteps;
 long     revolutions;
 boolean  oldA;
 boolean  oldB;
 boolean  newA;
 boolean  newB;
-int Aoutput = 2;
-int Boutput = 3;
+int Aoutput = 10;
+int Boutput = 11;
+int button = 9;
+boolean state = 0;
 
 void setup(){
   pinMode(Aoutput, INPUT);
   pinMode(Boutput, INPUT);
+  pinMode(button, INPUT);
   Serial.begin(9600);
 }
 
@@ -21,53 +23,59 @@ void loop(){
 
   if (oldA == 0 && newA == 1){
     if (newB == 0){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
     if (newB == 1){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
   };
-  
-  
+
   if (oldA == 1 && newA == 0){
     if (newB == 0){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
     if (newB == 1){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
   };
-  
+
   if (oldB == 0 && newB == 1){
     if (newA == 0){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
     if (newA == 1){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
   };
-  
+
   if (oldB == 1 && newB == 0){
     if (newA == 0){
-      steps = oldsteps + 1;
-      oldsteps = steps;
+      steps++;
     };
     if (newA == 1){
-      steps = oldsteps - 1;
-      oldsteps = steps;
+      steps--;
     };
   };
+<<<<<<< HEAD
 revolutions = steps / 64;
 if (digitalRead(7)==0){
   Serial.print(revolutions);
   delay(500);
 }
+=======
+  oldA = newA;
+  oldB = newB;
+
+  revolutions = (steps / (64));  
+  state = digitalRead(button);
+
+  if (state == 1) {
+    Serial.print(revolutions);
+    delay(300);
+  };
+
+>>>>>>> 91d99e7e24fa2135290ec816a8814256f3800ab6
 }
+
 
 
