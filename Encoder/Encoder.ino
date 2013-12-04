0788f82f53498357cdd6b83a767078fa3f2d3920
jpkale <jpkale@jpkserver.local>
2013-12-03 19:56:27 -0500

    Revert "Commit"
    This reverts commit 43d256d1929f1695a39bc3f3aa23a8ced9491f95, reversing
    changes made to 47b2232f0779bde2678c43318fde55a53816a942.

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -1,19 +1,17 @@
 //Written by Ward Prescott - 2013 - For Bucknell MARC
 long     steps;
+long     oldsteps;
 long     revolutions;
 boolean  oldA;
 boolean  oldB;
 boolean  newA;
 boolean  newB;
-int Aoutput = 10;
-int Boutput = 11;
-int button = 9;
-boolean state = 0;
+int Aoutput = 2;
+int Boutput = 3;
 
 void setup(){
   pinMode(Aoutput, INPUT);
   pinMode(Boutput, INPUT);
-  pinMode(button, INPUT);
   Serial.begin(9600);
 }
 
@@ -23,59 +21,53 @@ void loop(){
 
   if (oldA == 0 && newA == 1){
     if (newB == 0){
-      steps++;
+      steps = oldsteps + 1;
+      oldsteps = steps;
     };
     if (newB == 1){
-      steps--;
+      steps = oldsteps - 1;
+      oldsteps = steps;
     };
   };
-
+  
+  
   if (oldA == 1 && newA == 0){
     if (newB == 0){
-      steps--;
+      steps = oldsteps - 1;
+      oldsteps = steps;
     };
     if (newB == 1){
-      steps++;
+      steps = oldsteps + 1;
+      oldsteps = steps;
     };
   };
-
+  
   if (oldB == 0 && newB == 1){
     if (newA == 0){
-      steps--;
+      steps = oldsteps - 1;
+      oldsteps = steps;
     };
     if (newA == 1){
-      steps++;
+      steps = oldsteps + 1;
+      oldsteps = steps;
     };
   };
-
+  
   if (oldB == 1 && newB == 0){
     if (newA == 0){
-      steps++;
+      steps = oldsteps + 1;
+      oldsteps = steps;
     };
     if (newA == 1){
-      steps--;
+      steps = oldsteps - 1;
+      oldsteps = steps;
     };
   };
-<<<<<<< HEAD
 revolutions = steps / 64;
 if (digitalRead(7)==0){
   Serial.print(revolutions);
   delay(500);
 }
-=======
-  oldA = newA;
-  oldB = newB;
-
-  revolutions = (steps / (64));  
-  state = digitalRead(button);
-
-  if (state == 1) {
-    Serial.print(revolutions);
-    delay(300);
-  };
-
->>>>>>> 91d99e7e24fa2135290ec816a8814256f3800ab6
 }
-
 
 
