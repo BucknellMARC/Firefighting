267e8fd4831502b2e21910c44a322bc0e0bbdc10
Joe Kale <jpk017@bucknell.edu>
2014-01-26 21:26:57 -0500

    revisions to motor control

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -4,7 +4,8 @@ AF_DCMotor motorLeft(1);
 AF_DCMotor motorRight(3);
 long ticksLeft = 0;
 long ticksRight = 0;
-double TICK_MULT = .01318765572869;
+double TICK_MULT_STRAIT = .01318765572869;
+double TICK_MULT_TURN = TICK_MULT_STRAIT * 5.20871012;
 
 void setup(){
   Serial.begin(9600);
@@ -17,15 +18,24 @@ void setup(){
 }
 
 void loop(){
+  /* Put in serial code to get move commands here.
+     Code should wait for instruction, act, then send return signal. */
+  
   forward(10);
+  delay(1000);
+  backward(10);
+  delay(1000);
+  left(30);
+  delay(1000);
+  right(30);
   delay(1000);
 }
 
 void forward(int dist){
   do{
-    motorLeft.run(FORWARD);
+    motorLeft.run(BACKWARD);
     motorRight.run(FORWARD);
-  } while (ticksLeft * TICK_MULT < dist && ticksRight * TICK_MULT < dist);
+  } while (ticksLeft * TICK_MULT_STRAIT < dist && ticksRight * TICK_MULT_STRAIT < dist);
   motorLeft.run(RELEASE);
   motorRight.run(RELEASE);
   ticksLeft = 0;
@@ -36,7 +46,29 @@ void backward(int dist){
   do{
     motorLeft.run(BACKWARD);
     motorRight.run(BACKWARD);
-  } while (ticksLeft * TICK_MULT < dist && ticksRight * TICK_MULT < dist);
+  } while (ticksLeft * TICK_MULT_STRAIT < dist && ticksRight * TICK_MULT_STRAIT < dist);
+  motorLeft.run(RELEASE);
+  motorRight.run(RELEASE);
+  ticksLeft = 0;
+  ticksRight = 0;
+}
+
+void left(int deg){
+  do{
+    motorLeft.run(FORWARD);
+    motorRight.run(FORWARD);
+  } while (ticksLeft * TICK_MULT_TURN < deg && ticksRight * TICK_MULT_TURN < deg);
+  motorLeft.run(RELEASE);
+  motorRight.run(RELEASE);
+  ticksLeft = 0;
+  ticksRight = 0;
+}
+
+void right(int deg){
+  do{
+    motorLeft.run(FORWARD);
+    motorRight.run(BACKWARD);
+  } while (ticksLeft * TICK_MULT_TURN < deg && ticksRight * TICK_MULT_TURN < deg);
   motorLeft.run(RELEASE);
   motorRight.run(RELEASE);
   ticksLeft = 0;
