3892f75bc3d73d39fd7f09e7ef21326e42dc3b52
root <root@ward-linux3.(none)>
2014-01-27 02:46:58 -0500

    Added some preliminary flame sensor code, added directories for slave code and master code.

---
+++ Flamesensor/Flamesensor.ino
@@ -0,0 +1,22 @@
+void setup(){
+	int sensor1 = A0;
+	int sensor2	= A1;
+	int sensor3 = A2;
+	int sensor4 = A3;
+	int sensor5 = A4;
+
+	int state1;
+	int state2;
+	int state3;
+	int state4;
+	int state5;	
+}
+void loop() {
+	state1 = analogRead(sensor1);
+	state2 = analogRead(sensor2);
+	state3 = analogRead(sensor3);
+	state4 = analogRead(sensor4);
+	state5 = analogRead(sensor5);
+
+
+}
