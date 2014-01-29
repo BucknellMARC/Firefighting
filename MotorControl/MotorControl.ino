84cd386f8d853e2a89efc679d7ff4cd0e8ec4ea9
Greg Schrock <gls022@bucknell.edu>
2014-01-28 20:48:57 -0500

    Updated Motor Controllers

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -1,11 +1,14 @@
 #include <AFMotor.h>
 
+const int NEAR_DISTANCE = 5;
+
 AF_DCMotor motorLeft(1);
 AF_DCMotor motorRight(3);
 long ticksLeft = 0;
 long ticksRight = 0;
 double TICK_MULT_STRAIT = .01318765572869;
-double TICK_MULT_TURN = TICK_MULT_STRAIT * 5.20871012;
+//double TICK_MULT_TURN = TICK_MULT_STRAIT * 5.20871012;
+double TICK_MULT_TURN = TICK_MULT_STRAIT * 5.95;
 
 void setup(){
   Serial.begin(9600);
@@ -21,19 +24,13 @@ void loop(){
   /* Put in serial code to get move commands here.
      Code should wait for instruction, act, then send return signal. */
   
-  forward(10);
-  delay(1000);
-  backward(10);
-  delay(1000);
-  left(30);
-  delay(1000);
-  right(30);
-  delay(1000);
+  right(180);
+  delay(2000);
 }
 
 void forward(int dist){
   do{
-    motorLeft.run(BACKWARD);
+    motorLeft.run(FORWARD);
     motorRight.run(FORWARD);
   } while (ticksLeft * TICK_MULT_STRAIT < dist && ticksRight * TICK_MULT_STRAIT < dist);
   motorLeft.run(RELEASE);
@@ -71,12 +68,19 @@ void right(int deg){
   } while (ticksLeft * TICK_MULT_TURN < deg && ticksRight * TICK_MULT_TURN < deg);
   motorLeft.run(RELEASE);
   motorRight.run(RELEASE);
+  
+  motorLeft.run(BACKWARD);
+  motorRight.run(FORWARD);
+  delay(100);
+  motorLeft.run(RELEASE);
+  motorRight.run(RELEASE);
+  
   ticksLeft = 0;
   ticksRight = 0;
 }
 
 void countLeft(){
-  ticksLeft ++; 
+  ticksLeft ++;
 }
 
 void countRight(){
