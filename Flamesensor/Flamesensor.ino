c744af65254ef3cc5f57d500f1da245cab25f44f
Ward Prescott <ward@ward-linux3.(none)>
2014-02-04 19:38:02 -0500

    Ward's Commit

--- Flamesensor/Flamesensor.ino
+++ Flamesensor/Flamesensor.ino
@@ -1,3 +1,15 @@
+/* 
+Methodology:
+If any of the sensors read something over the base threshold the robot will halt.
+It will then take a closer look.
+If it determines that there is indeed a flame it will move to align itself with the center sensor.
+It will double check if it is alighned well.
+It will loop until it is aligned
+Then it will advance forward some distance, align again, and make sure that it is not over the closeness threshold for distance.
+It will repeat this until it gets to the required closeness.
+
+*/
+
 void setup(){
 	int sensor1 = A0;
 	int sensor2	= A1;
@@ -17,6 +29,5 @@ void loop() {
 	state3 = analogRead(sensor3);
 	state4 = analogRead(sensor4);
 	state5 = analogRead(sensor5);
-
 
 }
